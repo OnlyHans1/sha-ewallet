481404714f8b6918801328e6d68e408f0219843c
OnlyHans1 <adityarhaihan@gmail.com>
2024-12-02 18:20:39 +0700

    feat: profile menu item

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widgets/buttons.dart';
+import 'package:bank_sha/ui/widgets/profile_menu_item.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -42,9 +44,83 @@ class ProfilePage extends StatelessWidget {
             ),
             child: Column(
               children: [
-                
+                Container(
+                  width: 120,
+                  height: 120,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'assets/img_profile.png',
+                      ),
+                    ),
+                  ),
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 28,
+                      height: 28,
+                      decoration:
+                          BoxDecoration(shape: BoxShape.circle, color: whiteColor),
+                      child: Center(
+                        child: Icon(
+                          Icons.check_circle,
+                          color: greenColor,
+                          size: 24,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  'Rhaihan Aditya',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 18,
+                    fontWeight: medium,
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                ProfileMenuItem(
+                  iconUrl: 'assets/ic_edit_profile.png', 
+                  title: 'Edit Profile', 
+                  onTap: () {}
+                ),
+                ProfileMenuItem(
+                  iconUrl: 'assets/ic_pin.png', 
+                  title: 'My PIN', 
+                  onTap: () {}
+                ),
+                ProfileMenuItem(
+                  iconUrl: 'assets/ic_wallet.png', 
+                  title: 'Wallet Settings', 
+                  onTap: () {}
+                ),
+                ProfileMenuItem(
+                  iconUrl: 'assets/ic_reward.png', 
+                  title: 'My Rewards', 
+                  onTap: () {}
+                ),
+                ProfileMenuItem(
+                  iconUrl: 'assets/ic_help.png', 
+                  title: 'Help Center', 
+                  onTap: () {}
+                ),
+                ProfileMenuItem(
+                  iconUrl: 'assets/ic_logout.png', 
+                  title: 'Log Out', 
+                  onTap: () {}
+                ),
               ],
             ),
+          ),
+          const SizedBox(
+            height: 87,
+          ),
+          CustomTextButton(
+            title: 'Report a Problem',
+            onPressed: () {},
           ),
         ],
       ),
